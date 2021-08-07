fa7ef69fbdb98ca470c30f15f91ab1b40743ea97
jordano159 <yarden11111@gmail.com>
2021-08-07 15:55:54 +0300

    Enemy death effect

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var knockback = Vector2.ZERO
 onready var stats = $Stats
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
@@ -14,4 +15,10 @@ func _on_Hurtbox_area_entered(area):
 
 
 func _on_Stats_no_health():
+	create_enemy_death_effect()
 	queue_free()
+
+func create_enemy_death_effect():
+	var enemy_death_effect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemy_death_effect)
+	enemy_death_effect.global_position = global_position
--- World/Grass.gd
+++ World/Grass.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
+const GrassEffect = preload("res://Effects/GrassEffect.tscn")
+
 func create_grass_effect():
-	var GrassEffect = load("res://Effects/GrassEffect.tscn")
 	var grass_effect = GrassEffect.instance()
-	var world = get_tree().current_scene
-	world.add_child(grass_effect)
+	get_parent().add_child(grass_effect)
 	grass_effect.global_position = global_position
 
 func _on_Hurtbox_area_entered(area):
